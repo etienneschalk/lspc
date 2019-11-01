1033e32b7bcab1a95588ce01b183c52a8541cf22
Etienne <etienne.schalk@grenoble-inp.org>
2019-11-01 18:03:38 +0100

    Updating run_tests script

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -3,13 +3,19 @@
 # This script should be launched from the main folder of the project:
 # $ ./scripts/run_tests.sh
 
-echo "/--------------------------\\"
-echo "| Creating test folders... |"
-echo "\\--------------------------/"
+echo "/------------------------------------\\"
+echo "| Creating test folders and files... |"
+echo "\\------------------------------------/"
 ./scripts/make_test_folders.sh
 
 echo ""
-echo "/------------------\\"
-echo "| Running tests... |"
-echo "\\------------------/"
-python3 -m unittest -v
+echo "/-----------------------\\"
+echo "| Running unit tests... |"
+echo "\\-----------------------/"
+python3 -m unittest discover -s test/unit -v
+
+echo ""
+echo "/------------------------------\\"
+echo "| Running integration tests... |"
+echo "\\------------------------------/"
+python3 -m unittest discover -s test/integration -v
